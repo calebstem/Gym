2cf41f1b43062982bab46d5c496a9c431930bfaf
Caleb <calebstem@gmail.com>
2022-09-01 20:09:16 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,48 @@
+let popCount = 0
+let genPop = 0;
+
+let hourPop = []
+
+function arrayAdd(x) {
+  hourPop.push(x);
+  if (hourPop.length > 6) {
+    hourPop.length = 6;
+  }
+}
+
+const timeCard = {
+  inGym: 0,
+};
+
+document.getElementById('addUsr').onclick = function incPopCount(){
+  popCount++;
+}; 
+
+function transcribeTime(){
+  const date = new Date();
+  let minutes = date.getMinutes();
+ // let hour = date.getHours();
+  return minutes;
+}
+
+function timeCardObject(){
+  let timeMinutes = transcribeTime();
+  const tempCard = Object.create(timeCard);
+  tempCard.inGym = popCount;
+  tempCard.timeIn = timeMinutes;
+  arrayAdd(tempCard);
+  console.table(hourPop);
+  popCount = 0;
+}
+
+function calcPop(){
+  for (let i=0; i < hourPop.length; i++){
+    genPop += hourPop[i] * ((i+1)/hourPop.length);
+  }
+  console.log(genPop);
+  genPop = 0;
+}
+
+
+//setInterval(timeCardObject, 15000)
+
